51b19340ddf9c759ff22b634e2bd3bf7b7d698aa
Malmie <miechurska.m88@gmail.com>
2017-11-13 19:35:47 +0100

    new file

--- scripts.js
+++ scripts.js
@@ -1,14 +1,13 @@
 function drawTree(treeHeight) {
       
-  for ( i = 0 ; i <= treeHeight ; i++) {
+  for ( var i = 0 ; i < treeHeight ; i++) {
     var star = " ";
-      for ( j = 0 ; j === i ; j++ ) {
+      for ( var j = 0 ; j <= i ; j++ ) {
       	star += '*';
       }
     
      console.log(star);
   }
 }
-drawTree(1)
-drawTree(2)
+
 drawTree(5)
